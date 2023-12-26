11a9d28d6350e46e20f239a10d83513e0e3fff6b
Peter Swinkels <peter_swinkels@hotmail.com>
2023-12-26 17:17:21 +0100

    Add files via upload

--- Huffman Decompressor/Core.vb
+++ Huffman Decompressor/Core.vb
@@ -22,84 +22,94 @@ Public Module CoreModule
 
    'This procedure is executed when this program is started.
    Public Sub Main()
-      Dim CompressedData As New Queue(Of Byte)
-      Dim DecompressedData As List(Of Byte) = Nothing
-      Dim DecompressedSize As New Integer
-      Dim InputFile As String = GetCommandLineArgs().Last
-      Dim LookUpTree(&H0% To LOOK_UP_TREE_SIZE - &H1%) As Integer
-      Dim OutputFile As String = Path.Combine(Path.GetDirectoryName(GetCommandLineArgs().Last), Path.GetFileNameWithoutExtension(GetCommandLineArgs().Last))
+      Try
+         Dim CompressedData As New Queue(Of Byte)
+         Dim DecompressedData As List(Of Byte) = Nothing
+         Dim DecompressedSize As New Integer
+         Dim InputFile As String = GetCommandLineArgs().Last
+         Dim LookUpTree(&H0% To LOOK_UP_TREE_SIZE - &H1%) As Integer
+         Dim OutputFile As String = Path.Combine(Path.GetDirectoryName(GetCommandLineArgs().Last), Path.GetFileNameWithoutExtension(GetCommandLineArgs().Last))
 
-      If GetCommandLineArgs().Count = 2 Then
-         Console.WriteLine($"Decompressing {InputFile}...")
+         If GetCommandLineArgs().Count = 2 Then
+            Console.WriteLine($"Decompressing {InputFile}...")
 
-         Using CompressedFile As New BinaryReader(File.Open(InputFile, FileMode.Open, FileAccess.Read))
-            With CompressedFile
-               If .ReadBytes(SIGNATURE.Length).SequenceEqual(SIGNATURE) Then
-                  DecompressedSize = .ReadInt32()
+            Using CompressedFile As New BinaryReader(File.Open(InputFile, FileMode.Open, FileAccess.Read))
+               With CompressedFile
+                  If .ReadBytes(SIGNATURE.Length).SequenceEqual(SIGNATURE) Then
+                     DecompressedSize = .ReadInt32()
 
-                  For Index As Integer = LookUpTree.GetLowerBound(0) To LookUpTree.GetUpperBound(0)
-                     LookUpTree(Index) = .ReadUInt16()
-                  Next Index
+                     For Index As Integer = LookUpTree.GetLowerBound(0) To LookUpTree.GetUpperBound(0)
+                        LookUpTree(Index) = .ReadUInt16()
+                     Next Index
 
-                  Array.ForEach(.ReadBytes(CInt(.BaseStream.Length - .BaseStream.Position)), AddressOf CompressedData.Enqueue)
+                     Array.ForEach(.ReadBytes(CInt(.BaseStream.Length - .BaseStream.Position)), AddressOf CompressedData.Enqueue)
+                  Else
+                     Console.WriteLine("The specified file is not a Huffman compressed file.")
+                  End If
+               End With
+            End Using
+
+            If CompressedData.Count > 0 Then
+               Console.WriteLine($"Compressed size: {CompressedData.Count}")
+               Console.WriteLine($"Decompressed size: {DecompressedSize}")
+
+               DecompressedData = Decompress(CompressedData, LookUpTree, DecompressedSize)
+
+               If DecompressedData IsNot Nothing AndAlso DecompressedData.Count = DecompressedSize - 1 Then
+                  File.WriteAllBytes(OutputFile, DecompressedData.ToArray())
+                  Console.WriteLine("Done.")
                Else
-                  Console.WriteLine("The specified file is not a Huffman compressed file.")
+                  Console.WriteLine("Error decompressing file.")
                End If
-            End With
-         End Using
-
-         If CompressedData.Count > 0 Then
-            Console.WriteLine($"Compressed size: {CompressedData.Count}")
-            Console.WriteLine($"Decompressed size: {DecompressedSize}")
-
-            DecompressedData = Decompress(CompressedData, LookUpTree, DecompressedSize)
-
-            If DecompressedData.Count = DecompressedSize - 1 Then
-               File.WriteAllBytes(OutputFile, DecompressedData.ToArray())
-               Console.WriteLine("Done.")
             Else
-               Console.WriteLine("Error decompressing file.")
+               Console.WriteLine($"Syntax: {GetCommandLineArgs().First} <HUFF file>")
             End If
          Else
-            Console.WriteLine($"Syntax: {GetCommandLineArgs().First} <HUFF file>")
+            Console.WriteLine("No Huffman compressed data to decompress.")
          End If
-      Else
-         Console.WriteLine("No Huffman compressed data to decompress.")
-      End If
+      Catch ExceptionO As Exception
+         Console.WriteLine($"Error: {ExceptionO.Message}")
+      End Try
    End Sub
 
    'This procedure decompresses the specified data and returns the result.
    Private Function Decompress(CompressedData As Queue(Of Byte), LookUpTree() As Integer, DecompressedSize As Integer) As List(Of Byte)
-      Dim Bit As Integer = &H1%
-      Dim ByteO As New Byte
-      Dim DecompressedData As New List(Of Byte)
-      Dim Leaf As Integer = ROOT
-      Dim LookUpTreeItem As New Integer
+      Try
+         Dim Bit As Integer = &H1%
+         Dim ByteO As New Byte
+         Dim DecompressedData As New List(Of Byte)
+         Dim Leaf As Integer = ROOT
+         Dim LookUpTreeItem As New Integer
 
-      ByteO = CompressedData.Dequeue()
-      Do While (DecompressedData.Count < DecompressedSize - 1)
-         If (ByteO And Bit) = &H0% Then
-            If Leaf > LOOK_UP_TREE_SIZE Then Exit Do
-            LookUpTreeItem = LookUpTree(Leaf)
-         Else
-            If Leaf + &H1% > LOOK_UP_TREE_SIZE Then Exit Do
-            LookUpTreeItem = LookUpTree(Leaf + &H1%)
-         End If
+         ByteO = CompressedData.Dequeue()
+         Do While (DecompressedData.Count < DecompressedSize - 1)
+            If (ByteO And Bit) = &H0% Then
+               If Leaf > LOOK_UP_TREE_SIZE Then Exit Do
+               LookUpTreeItem = LookUpTree(Leaf)
+            Else
+               If Leaf + &H1% > LOOK_UP_TREE_SIZE Then Exit Do
+               LookUpTreeItem = LookUpTree(Leaf + &H1%)
+            End If
 
-         Bit = (Bit << &H1%) And &HFFFF%
-         If Bit > Byte.MaxValue Then
-            Bit = &H1%
-            ByteO = CompressedData.Dequeue()
-         End If
+            Bit = (Bit << &H1%) And &HFFFF%
+            If Bit > Byte.MaxValue Then
+               Bit = &H1%
+               ByteO = CompressedData.Dequeue()
+            End If
 
-         If (LookUpTreeItem And LOOK_UP_TREE_TAG) = LOOK_UP_TREE_TAG Then
-            Leaf = (LookUpTreeItem And &HFF%) << &H1%
-         Else
-            DecompressedData.Add(ToByte(LookUpTreeItem And &HFF%))
-            Leaf = ROOT
-         End If
-      Loop
+            If (LookUpTreeItem And LOOK_UP_TREE_TAG) = LOOK_UP_TREE_TAG Then
+               Leaf = (LookUpTreeItem And &HFF%) << &H1%
+            Else
+               DecompressedData.Add(ToByte(LookUpTreeItem And &HFF%))
+               Leaf = ROOT
+            End If
+         Loop
 
-      Return DecompressedData
+         Return DecompressedData
+      Catch ExceptionO As Exception
+         Console.WriteLine($"Error: {ExceptionO.Message}")
+      End Try
+
+      Return Nothing
    End Function
 End Module
